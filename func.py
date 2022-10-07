3d0ee04c3b04f36e21e0864c28ea621162520fa4
siprata <siva.pratap@oracle.com>
2022-10-07 20:47:03 +0000

    test

--- func.py
+++ func.py
@@ -1,14 +1,59 @@
-# Import the Libararies
-import pandas as pd
+{ "funcMode": "describeFunction"}
 
-########################################################################################################################
-#Read the Files
+funcDefinition = {
+    "status": {
+        "returnCode": 0,
+        "errorMessage": ""
+    },
+    "funcDescription": {
+        "outputs": [
+            {"name": "word_count", "dataType": "integer"}
+        ],
+        "parameters": [
+            {"name": "textColumn", "displayName": "Text Column",
+             "description": "Choose column to count words", "required": True,
+             "value": {"type": "column"}}
+        ],
+        "bucketName": "bucket-OCI-FAAS",
+        "isOutputJoinableWithInput": True
+    }
+}
 
-#NETWORK
-df1 = pd.read_excel("oci://CloudWorldDemo/Input_Test.xlsx")
+def handler(ctx, data: io.BytesIO = None):
+    response_data = ""
+    try:
+        body = json.loads(data.getvalue())
+        funcMode = body.get("funcMode")
+        if funcMode == 'describeFunction':
+           response_data = json.dumps(funcDefinition)
+    except (Exception, ValueError) as ex:
+        response_data = json.dumps(
+            {"error": "{0}".format(str(ex))})
+    return response.Response(
+        ctx, response_data,
+        headers={"Content-Type": "application/json"}
+    )
 
-#Assign to another file
-
-result=df1
-
-result.to_excel("oci://CloudWorldDemo/Output_Test.xlsx")
+{ "funcMode": "executeFunction"}
+	
+{
+    "args":
+    {
+        "textColumn": "Ename"
+    },
+    "funcMode": "executeFunction",
+    "input":
+    {
+        "bucketName": "bucket-OCI-FAAS",
+        "fileExtension": ".xlsx",
+        "fileName": "Input_Test",
+        "method": "xlsx",
+        "rowID": "Eno"
+    },
+    "output":
+    {
+        "bucketName": "bucket-OCI-FAAS",
+        "fileExtension": ".xlsx",
+        "fileName": "Output_Test"
+    }
+}
